6b46c572680a5922b691a1c4031fe67b99b46e70
Hello! <93799947+bcolby94@users.noreply.github.com>
2022-03-18 18:19:11 -0400

    First commit

---
+++ public/script.js
@@ -0,0 +1,68 @@
+let socket;
+
+const emitPost = () => {
+  if (socket.readyState !== 1) {
+      alert("Socket not connected. Please try again.");
+  }
+  const message = document.getElementById('content').value;
+  document.getElementById('content').value = '';
+  socket.send([message]);
+};
+
+document.querySelector('#chatbar').addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    emitPost();
+  }
+});
+
+const init = () => {
+  // websocket
+  socket = new WebSocket('ws://' + location.host);
+  // Log errors to the console for debugging.
+  socket.onerror = function(error) {
+    console.log(error);
+  };
+  // Reconnect upon disconnect.
+  socket.onclose = function() {
+    console.log(`Your socket has been disconnected. Attempting to reconnect...`);
+    setTimeout(function() {
+      init();
+    }, 1000);
+  };
+  socket.onmessage = (message) => {
+    const parsedData = JSON.parse(message.data);
+
+    /* Inserts regular messages into DOM */
+    if (parsedData.alert) {
+      const num = Math.floor((Math.random() * 9) + 1);
+      const areaNum = `x${num}`;
+      const areaDOM = document.getElementById(areaNum);
+      areaDOM.innerHTML = parsedData.alert;
+      msg = new SpeechSynthesisUtterance();
+      msg.text = parsedData.alert;
+      window.speechSynthesis.speak(msg);
+    }
+    /* End */
+
+    /* Inserts video messages into DOM */
+    if (parsedData.html) {
+      newVideo = parsedData.html;
+      const num = Math.floor((Math.random() * 9) + 1);
+      const areaNum = `x${num}`;
+      const container = document.createElement('div');
+      container.setAttribute('id', newVideo);
+      videoLink = `https://www.youtube.com/embed/${newVideo}?autoplay=1`;
+      container.innerHTML = `
+        <iframe height="100%" width="100%" src="${videoLink}" allow="autoplay; encrypted-media" allowfullscreen />
+      `;
+      document.getElementById(areaNum).appendChild(container);
+    /* End */
+    }
+  }
+
+    socket.onopen = () => {
+      console.log('client connected successfully');
+    };
+  }
+
+init();
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express().enable('trust proxy');
+const http = require('http');
+const webServer = http.createServer(app);
+const cors = require('cors');
+const WebSocketServer = require('ws').Server;
+const clients = new Map();
+const ws = new WebSocketServer({
+  server: webServer,
+});
+
+process.on('uncaughtException', function (err) { // I don't like killing the process when one of my libraries messes up.
+  console.log('UNCAUGHT EXCEPTION\n' + err)
+});
+
+ws.on('connection', function connection(ws, req) {
+  const clientIP = req.connection.remoteAddress;
+  const iden = Math.floor(Math.random() * 999999);
+  clients.set(iden, {
+    socket: ws,
+    IP: clientIP,
+  });
+  ws.on('message', function incoming(message) {
+    message = message.toString();
+    if(message.slice(0, 4) == "http"){
+      wsHtml(message);
+    } else {
+      wsAlert(message);
+    }   
+  });
+});
+
+const wsBroadcast = (data) => {
+  clients.forEach(function (client) {
+    if (client.socket.readyState == 1) {
+      client.socket.send(data);
+    }
+  });
+};
+
+const wsAlert = (alertStr) => {
+  const newAlert = JSON.stringify({
+    alert: alertStr
+  });
+  wsBroadcast(newAlert);
+};
+
+const wsHtml = (link) => {
+  console.log(link);
+  let html = link;
+  const videoId = html.slice(html.length - 11, html.length);
+  html = `https://www.youtube.com/watch?v=${videoId}`;
+      const newData = JSON.stringify({
+        html: videoId,
+      });
+      wsBroadcast(newData);
+  }
+
+app.use(cors());
+app.use('/', express.static(__dirname + '/public/'));
+webServer.listen(7004, function listening() {
+  console.log('Listening on %d', webServer.address().port);
+});
